64e16ef9152e09a372b5fbd151158dc385a2913a
Andrei Helo <andrei.helo@points.com>
2019-06-28 18:13:36 -0600

    Class 1
    Date: June 25th, 2019

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_06_26_012358) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.integer "max_students"
+    t.text "description"
+  end
 
 end
